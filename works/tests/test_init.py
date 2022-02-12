dc710b21377a5bd077f8818143bfd482985126d5
RiwsPy <thibault.debruyne@hotmail.fr>
2022-02-12 17:10:33 +0100

    correcting tests

--- works/tests/test_init.py
+++ works/tests/test_init.py
@@ -15,6 +15,8 @@ def test_convert_osm_to_geojson():
     w.load('mock_osm')
     w2 = Works()
     w2.load('mock_geojson')
+    print(w)
+    print(w2)
     assert convert_osm_to_geojson(w) == w2
 
     del w['features']
