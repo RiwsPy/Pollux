3f7419da22a704c5425c1f2c8ccacad1705adbfa
RiwsPy <thibault.debruyne@hotmail.fr>
2022-02-13 10:44:04 +0100

    adding bus_way

--- works/trees.py
+++ works/trees.py
@@ -5,7 +5,7 @@ from api_ext import grenoble_alpes_metropole
 class Trees(Works):
     filename = 'trees'
     request_method = grenoble_alpes_metropole.call
-    url = 'opendata/38185-GRE/EspacePublic/json/ARBRES_TERRITOIRE_VDG_EPSG4326.json'
+    url = '/opendata/38185-GRE/EspacePublic/json/ARBRES_TERRITOIRE_VDG_EPSG4326.json'
     query = ""
-    COPYRIGHT_ORIGIN = 'www.metropolegrenoble.org'
+    COPYRIGHT_ORIGIN = grenoble_alpes_metropole.BASE_URL
     COPYRIGHT_LICENSE = 'ODbL'
