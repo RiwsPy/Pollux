00bfe5251b989aec931262ddc3a34b14ba958f7f
RiwsPy <thibault.debruyne@hotmail.fr>
2022-03-23 16:28:36 +0100

    adding map_desc.html & base.html

--- website/views.py
+++ website/views.py
@@ -6,8 +6,6 @@ from . import app
 from flask import render_template, jsonify, request
 from api_ext.clips import Clips
 from api_ext import BadStatusError
-from works.trees import Trees
-from works.crossings import Crossings
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -66,3 +64,41 @@ def mentions_legales():
 @app.route('/encyclopedia/', methods=['GET'])
 def encyclopedia():
     return render_template('encyclopedia.html')
+
+
+DESC1 = """
+Tuto ?
+
+Sur cette carte vous êtes invités à créer votre propre zone de recherche.
+Une fois fait, Pollux identifie les différents éléments présents dans cette zone.
+Dans le panneau de droite, s'affichera des recommandations d'éclairage en fonction de ceux-ci.
+
+En passant votre souris sur la forme créée, s'affichera le nombre des différents éléments trouvés.
+"""
+
+DESC2 = """
+Pleins de trucs ici, trop faciles à lire.
+"""
+
+DESC3 = """
+Des choses encore mieux là.
+"""
+
+MAP_NB_TO_DATA = {
+    "1": {
+        "lines": DESC1.split('\n'),
+        "button_url": '/map'},
+    "2": {
+        "lines": DESC2.split('\n'),
+        "button_url": '/conflictmap'},
+    "3": {
+        "lines": DESC3.split('\n'),
+        "button_url": '/conflictTreeLum'}
+}
+
+
+@app.route('/map_desc/<map_nb>', methods=['GET'])
+def show_map_description(map_nb):
+    return render_template('map_desc.html',
+                           button_txt='Accéder à la carte',
+                           **MAP_NB_TO_DATA.get(map_nb, {}))
