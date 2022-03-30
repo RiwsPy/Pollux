400dc3c34e3491e7edb225d7b5765f459540303a
RiwsPy <thibault.debruyne@hotmail.fr>
2022-03-30 19:41:42 +0200

    removing unused files

--- api_ext/openobs.py
+++
@@ -1,9 +0,0 @@
-from . import call as base_call
-
-# datasets : nb de jeu de db
-# occurrences : nb d'observation
-
-def call(self, url: str, **kwargs) -> dict:
-    url = "https://openobs.mnhn.fr/api/" + url
-
-    return base_call(self, url=url, method="get")
