0c422291acdde294d87a78a70b77e46320cc2e5c
RiwsPy <thibault.debruyne@hotmail.fr>
2022-03-30 19:18:58 +0200

    adding -uDB command

--- engine.py
+++ engine.py
@@ -6,38 +6,40 @@ from works.parks import Parks
 from works.shops import Shops
 from works.tc_ways import Tc_ways
 from works.tc_stops import Tc_stops
-from works.lamps import Lamps, Lamps_night
+from works.highways import Highways
+from works.lamps import Lamps
 from website import app
 from works.accidents import Accidents
 from dotenv import load_dotenv
 from formats.position import Position
-from formats.geojson import Geojson, Feature
 from pathlib import Path
-import os
-from json import dump
+import argparse
 
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent
+db_classes = (Crossings, Parks, Shops, Tc_stops, Tc_ways, Trees, Accidents, Birds, Lamps, Highways)
 
 
-def update_db_after_map_position():
-    cls_set_with_req = (Crossings, Parks, Shops, Tc_stops, Tc_ways, Trees)
-    cls_set_load_without_request = (Accidents, Birds)
-    for cls_type in cls_set_with_req:
-        print(cls_type, 'en cours.')
+def full_update():
+    for cls_type in db_classes:
+        update(cls_type)
+    print('Mise à jour terminée.')
+
+
+def update(cls_type):
+    print(cls_type, 'en cours.')
+    try:
         cls_instance = cls_type()
         data = cls_instance.request()
         cls_instance.update(data)
-        cls_instance.dump()
-        cls_instance.output()
-
-    for cls_type in cls_set_load_without_request:
-        print(cls_type, 'en cours.')
-        cls_instance = cls_type()
-        cls_instance.load()
-        cls_instance.output()
+        if cls_instance.fake_request is False:
+            cls_instance.dump()  # save request result
+        cls_instance.output()  # filter on request result and save it
+    except FileNotFoundError:
+        print('Error', cls_type, ': FileNotFound')
 
 
+# TODO: replacer ce bazar
 def team_conflict(blue_team: list, red_team: list) -> None:
     if not blue_team or not red_team:
         return None
@@ -60,28 +62,52 @@ def team_conflict(blue_team: list, red_team: list) -> None:
         red_member = cls_type()
         red_member.load(red_member.output_filename, file_ext='json')
         red_features['features'].extend(red_member['features'])
-        red_team_name += '_' + red_features.filename
+        red_team_name += '_' + red_member.filename
 
-    features = Geojson()
-    nb = 0
+    blue_features['COPYRIGHT'] = 'The data is made available under ODbL.'
     for blue_feature in blue_features:
         cr_position = Position(blue_feature['geometry']['coordinates'])
-        for red_feature in red_features:
-            nb += 1
-            if nb%1000000 == 0:
-                print(round(nb*100/114871985, 2), '% éléments pris en compte.')
-            distance_between = cr_position.distance(red_feature['geometry']['coordinates'])
-            if distance_between <= 25:
-                new_feature = Feature()
-                new_feature.lat, new_feature.lng = (cr_position + red_feature['geometry']['coordinates'])/2
-                new_feature.intensity = round(1 - (distance_between**2)/(25**2), 2)
-                features.append(new_feature)
+        blue_feature['properties']['intensity'] = {'base': 0, 'day': 0, 'night': 0}
 
-    with open(os.path.join(BASE_DIR, 'db/' 'conflict_' + blue_team_name + '__' + red_team_name + '.json'), 'w+') as file:
-        dump(features, file)
+        calc_base = True
+        calc_day = int(blue_feature['properties'].get('Lampe - Température Couleur') or '5000') > 2500 and \
+            blue_feature['properties'].get('Lampe - Régime (simplifié)') != "Détéction en milieu de nuit"
+        calc_night = blue_feature['properties'].get('Lampe - Régime (simplifié)') != "Abaissement en milieu de nuit"
+
+        for red_feature in red_features:
+            geo_distance_between = cr_position.distance(red_feature['geometry']['coordinates'])
+            diff_lum_tree_height = max(int(blue_feature['properties'].get("Lampe - Hauteur de feu", "10")) - 5, 0)
+            square_distance = diff_lum_tree_height ** 2 + geo_distance_between ** 2
+            if square_distance <= 25 ** 2:
+                item_intensity = blue_feature['properties']['intensity']
+                # valeur multipliée par 9 pour avoir des valeurs comprises entre 0 et 1
+                # TODO: à revoir
+                intensity_value = 9 / square_distance
+                if calc_base:
+                    item_intensity['base'] += intensity_value
+                    if calc_day:
+                        item_intensity['day'] += intensity_value
+                        if calc_night:
+                            item_intensity['night'] += intensity_value
+
+    blue_features.dump('conflict_' + blue_team_name + '__' + red_team_name + '.json')
 
 
 if __name__ == '__main__':
-    #update_db_after_map_position()
-    #team_conflict(blue_team=[Lamps_night], red_team=[Trees, Birds])
-    app.run()
+    parser = argparse.ArgumentParser(description='Pollux - Fonctionnalités.')
+    parser.add_argument("-uDB", "--updateDB",
+                        nargs='*',
+                        choices=['all'] + [cls.filename for cls in db_classes],
+                        help="Mettre à jour les bases de données de l'application.")
+    args = parser.parse_args()
+
+    if args.updateDB is not None:
+        db_args = [arg.lower() for arg in args.updateDB]
+        if not db_args or "all" in db_args:
+            full_update()
+        else:
+            for cls in set(db_classes).intersection(set(db_args)):
+                update(cls)
+    else:
+        # team_conflict(blue_team=[Lamps], red_team=[Trees, Birds])
+        app.run()
