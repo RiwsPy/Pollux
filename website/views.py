306c7e9464e3db9f6a2523ae030588f0412ea0de
RiwsPy <thibault.debruyne@hotmail.fr>
2022-04-01 14:06:54 +0200

    enrichissement de base.htlm, peaufinage

--- website/views.py
+++ website/views.py
@@ -15,21 +15,30 @@ MAP_NB_TO_DATA = {
     "1": {
         "lines": DESC1.split('\n'),
         "template_name_or_list": 'maps.html',
-        'script_filename': 'js/leafmap.js'},
+        'script_filename': 'js/leafmap.js',
+        'title': 'Carte des recommandations',
+        'icon': 'button_recommandation.png'},
     "2": {
         "lines": DESC2.split('\n'),
         "template_name_or_list": 'heatmaps.html',
-        'script_filename': 'js/conflictTreeCrossing.js'},
+        'script_filename': 'js/conflictTreeCrossing.js',
+        'title': 'Carte de contradiction',
+        'icon': 'button_contradiction.png'},
     "3": {
         "lines": DESC3.split('\n'),
         "template_name_or_list": 'heatmaps.html',
-        'script_filename': 'js/conflictTreeLum.js'}
+        'script_filename': 'js/conflictTreeLum.js',
+        'title': "Carte d'impact",
+        'icon': 'button_impact.png'}
 }
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', is_mainpage=True)
+    return render_template('index.html',
+                           is_mainpage=True,
+                           page_title="Accueil",
+                           map_data=MAP_NB_TO_DATA)
 
 
 @app.route('/map/<map_nb>')
@@ -37,6 +46,7 @@ def show_map(map_nb):
     if MAP_NB_TO_DATA.get(str(abs(int(map_nb)))):
         return render_template(**MAP_NB_TO_DATA.get(str(abs(int(map_nb))), {}))
     return index()
+
 
 @app.route('/api/<filename>', methods=['GET'])
 def print_json(filename):
@@ -66,17 +76,23 @@ def clips_recommendation():
 
 @app.route('/mentions_legales/', methods=['GET'])
 def mentions_legales():
-    return render_template('mentions_legales.html')
+    return render_template('mentions_legales.html',
+                           page_title="Mentions légales",
+                           map_data=MAP_NB_TO_DATA)
 
 
 @app.route('/encyclopedia/', methods=['GET'])
 def encyclopedia():
-    return render_template('encyclopedia.html')
+    return render_template('encyclopedia.html',
+                           page_title="Encyclopédie",
+                           map_data=MAP_NB_TO_DATA)
 
 
-@app.route('/map_desc/<map_nb>', methods=['GET'])
-def show_map_description(map_nb):
+@app.route('/map_desc/<map_id>', methods=['GET'])
+def show_map_description(map_id):
     return render_template('map_desc.html',
-                           button_txt='Accéder à la carte',
-                           lines=MAP_NB_TO_DATA[map_nb]['lines'],
-                           button_url='/map/'+map_nb)
+                           map_id=map_id,
+                           lines=MAP_NB_TO_DATA[map_id]['lines'],
+                           button_url='/map/'+map_id,
+                           page_title=MAP_NB_TO_DATA[map_id].get('title'),
+                           map_data=MAP_NB_TO_DATA)
