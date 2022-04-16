d10a75426c5e30e4ed54dfd56355e04cead16e0a
RiwsPy <thibault.debruyne@hotmail.fr>
2022-04-16 22:27:05 +0200

    updating lamp regime

--- works/lamp.py
+++ works/lamp.py
@@ -2,12 +2,29 @@ from . import Default_works
 from api_ext.grenoble_alpes_metropole import Gam
 
 lowering_night_impact = {
+    "": 0,
+    "GRE NOCTURNE": 0,
+    "CREM NOCTURNE": 0,
+    "4150h": 0,
+    "GRE PERMANENT": 0,
+    "CREM PERMANENT": 0,
+    "GRE AUTONOME": 0,  # ??
+    "GRE NOCTURNE AVEC TELEGESTION": 0,
+    "CREM NOCTURNE AVEC DETECTION  10%": 0,
+    "CREM NOCTURNE AVEC DETECTION  20%": 0,
+    "GRE NOCTURNE PROLONGE 2H (1H Avant +1H Aprés)": 0,
+    "CREM NOCTURNE AVEC COUPURE ST2": 0,  # TODO !?
     "CREM NOCTURNE AVEC REDUCTION 33% flux - 33 % NRJ en milieu de nuit (LED)": 33,
     "CREM NOCTURE AVEC REDUCTION 50% flux - 40 % NRJ en milieu de nuit": 50,
     "CREM NOCTURNE AVEC REDUCTION 50% flux - 50 % NRJ en milieu de nuit (LED)": 50,
     "GRE NOCTURNE AVEC REDUCTION Bi-Pw (Réduction de 30% de 22h40 à 5h40)": 30,
+    "CREM NOCTURNE AVEC VARIATIEUR (Réduction de  50 % flux - 40 % puissance de 23h à 6h)": 50,
+    "CREM NOCTURNE AVEC REDUCTION 80% flux - 80 % NRJ en milieu de nuit (LED)": 80,
+    "CREM NOCTURNE AVEC REDUCTION 50% de 22h à 0h et de 70% de 0h à 5h et 50% de 5h à 6h": 50,  # TODO
     "GRE NOCTURNE AVEC REDUCTION VARIATEUR LUBIO (Réduction de 30 % de 23h à 6h)": 30,
     "CREM NOCTURNE AVEC VARIATIEUR BH (Réduction de  33 % flux - 27 % puissance de 22h à 6h)": 33,
+    "CREM NOCTURE AVEC REDUCTION 33% flux - 27 % NRJ en milieu de nuit": 33,
+    "GRE NOCTURNE AVEC COMPTAGE (Eclairage à la demande)": 0,
 }
 
 
